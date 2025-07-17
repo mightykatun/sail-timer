5221f0ef4be58367ac67c6fc8c0624f98ad7ed3b
Dionys Kaloulis <generalhellothere55555@gmail.com>
2025-07-17 12:03:11 +0200

    Created and tested boat_list_add()

--- json_functions.py
+++ json_functions.py
@@ -2,34 +2,72 @@ import json
 import os
 import time as tm
 
-def init_race_session(race_name: str, boat_dict: dict):
+
+
+def init_race_session(race_name: str, boat_list: list):
     '''
     Initialize the race_name.json file that will serve as data storage for the whole race
 
     Args:
         race_name (str): Name of the race, e.g. "Critérium Manche 1"
-        boat_dict (dict): A dict containing {"Boat name": {"Sail number": "SUI ...", "Category": "Lestés A"}, ...}
+        boat_dict (dict): A list containing [{"Name": "Little Nemo II", "Sail number": "SUI 102", "Category": "Lestés A"}, ...]
     
     Returns:
         False: If a race already exists
         True: Otherwise
     '''
+
     # check if race exists
     if os.path.exists(f"./{race_name}.json"):
         return False
+    
     # create the race's json file
     else:
         with open(f"./{race_name}.json", "w") as race_file:
-            init_dict = {"Race name": race_name, "Participants": boat_dict, "Started": False}
+            init_dict = {"Race name": race_name, "Participants": boat_list, "Started": False}
             json.dump(init_dict, race_file, indent=4)
         return True
     
 
+
 def boat_list_add(race_name: str, boat_info_to_add: dict):
-    pass
+    '''
+    Adds one boat to the list
+
+    Args:
+        race_name (str): Name of the race, e.g. "Critérium Manche 1"
+        boat_info_to_add (dict): {"Name": "Little Nemo II", "Sail number": "SUI 102", "Category": "Lestés A"}
+    
+    Returns:
+        False: If race is started
+        True: Otherwise
+    '''
+    # check if the race is already started
+    with open(f"./{race_name}.json", "r") as race_file:
+        race_dict = json.load(race_file)
+    if race_dict["Started"] != False:
+        return False
+    
+    # update the boat list
+    else:
+        with open(f"./{race_name}.json", "w") as race_file:
+            race_dict["Participants"].append(boat_info_to_add)
+            json.dump(race_dict, race_file, indent=4)
+        return True
+
+
 
 def boat_list_del(race_name: str, boat_to_delete: str):
+    '''
+    '''
+
+    # check if the race is already started
+    with open(f"./{race_name}.json", "r") as race_file:
+        race_dict = json.load(race_file)
+    if race_dict["Started"] != False:
+        return False
     pass
+
 
 
 def start_procedure(race_name: str):
@@ -43,11 +81,13 @@ def start_procedure(race_name: str):
         False: If procedure already started
         True: Otherwise
     '''
+
     # check if the race is already started
     with open(f"./{race_name}.json", "r") as race_file:
         race_dict = json.load(race_file)
     if race_dict["Started"] != False:
         return False
+    
     # start the race
     else:
         with open(f"./{race_name}.json", "w") as race_file:
--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from json_functions import init_race_session, start_procedure, boat_list_add, bo
 
 
 if __name__ == "__main__":
+
+
+    ### TESTING
+
     race_name = "Critérium Manche 1"
-    boat_dict = {"melges 24": {"Sail": "SUI 1459", "Category": "B"}, "little": {"Sail": "SUI 102", "Category": "A"}}
+    boat_dict = [{"Name": "Melges 24", "Sail": "SUI 1459", "Category": "B"}, {"Name": "Nemo", "Sail": "SUI 102", "Category": "A"}]
 
     init_race_session(race_name, boat_dict)
+
+    tm.sleep(2)
+
+    boat_list_add(race_name, {"Name": "Whooo", "Sail": "SUI 1004", "Category": "B"})
 
     tm.sleep(2)
 
